1b13b931d3d20e1288bdcf6c43f800a58266bb92
PowerLevel9000 <116451144+PowerLevel9000@users.noreply.github.com>
2023-03-23 20:23:07 +0530

    homepage styling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -29,27 +29,39 @@ const Home = () => {
       <div className="companies">
         {matrics.map((item, index) => (
           <div className={(index % 4 === 0 || index % 4 === 3 || index === 1) ? 'company' : 'company dark'} key={item.symbol}>
+            <div className="indicator">
+              {item.status < 0 ? <i class="fa-sharp fa-solid fa-arrow-down "></i> : <i class="fa-sharp fa-solid fa-arrow-up "></i>}
+            </div>
             <div className="logoWraper">
               <img src={item.status < 0 ? bear : bull} alt="" />
             </div>
             <div className="intro">
-              <span>{item.symbol}</span>
+              <div className="symbol">
+                <span>{item.symbol}</span>
+                <Link to="/details" ><i className="fa-solid fa-arrow-right" onClick={() => dispatch(setQuerry(item.symbol))} /></Link>
+              </div>
               <h2>{item.name}</h2>
               <div className="pricing">
-                <strong>
+                <strong className="price">
                   Price : $
+                  {' '}
                   {item.price}
                 </strong>
-                <span>
-                  Price Change : $
-                  {item.status}
-                </span>
-                <span>
-                  Price Change : %
-                  {item.statusPercentage}
-                </span>
+                <div className="status">
+                  <span>
+                    Price Change : $
+                    <span style={item.status < 0 ? { color: 'white',  } : { color: 'green' }}>
+                      {' '}
+                      {item.status}
+                    </span>
+                  </span>
+                  <span>
+                    Price Change% : 
+                    {' '}
+                    {item.statusPercentage}
+                  </span>
+                </div>
               </div>
-              <Link to="/details" ><i className="fa-solid fa-arrow-right" onClick={() => dispatch(setQuerry(item.symbol))} /></Link>
             </div>
           </div>
         ))}
@@ -60,10 +72,10 @@ const Home = () => {
 
 const HomeWraper = styled.div`
   margin: 0;
-  padding: 0;
   .carouselWrapper {
     position: relative;
   }
+
   label {
     display: block;
     background-color: var(--darkSub);
@@ -86,6 +98,7 @@ const HomeWraper = styled.div`
     width: 50%;
     display: flex;
     justify-content: space-around;
+    position: relative;
     box-shadow:
       0px 0px 1px rgba(234, 76, 137, 0.144),
       0px 0px 1px rgba(234, 76, 137, 0.64)
@@ -100,6 +113,97 @@ const HomeWraper = styled.div`
     }
     .intro {
       width: 70%;
+      padding: 1rem; {
+      .symbol {
+        display: flex;
+        justify-content: space-between;
+        i {
+          font-size: 1.5rem;
+          border: 1px solid white;
+          padding: 0.5rem;
+          border-radius: 50%;
+          color: white;
+        }
+      }
+      }
+
+      h2 {
+        margin: 0.5rem 0;
+      }
+
+      .price {
+        font-size: 1.2rem;
+        color: green;
+      }
+
+      .status {
+        width: 70%;
+        display: flex;
+        justify-content: space-between;
+      }
+    }
+
+    .indicator {
+      position:absolute;
+      right: 10%;
+      top: 50%;
+      font-size: 2rem;
+
+      .fa-arrow-down {
+        color: red;
+      }
+      .fa-arrow-up {
+        color: green;
+      }
+    }
+
+    @media screen and (max-width: 768px) {
+      width: 50%;
+      flex-direction: column;
+      .intro {
+        padding: 0.5rem 0 0.5rem 0.5rem;
+      }
+      .logoWraper {
+        width:100%;
+        height: 80%;
+        padding: 0.1rem 0;
+        display: grid;
+        place-items: center;
+        img {
+          width: 80%;
+          height: 80%;
+        }
+      }
+
+      .intro {
+        width: 100%;
+        font-size: 1.1rem;
+        h2 {
+          margin: 0.2rem 0;
+        }
+      }
+
+      .symbol {
+        align-items: center;
+        padding-right: 1rem;
+      }
+
+      .pricing {
+        width: 100%;
+
+        .status {
+          width: 95%;
+        }
+      }
+
+      .indicator {
+        right: 30%;
+        top: 67%;
+      }
+    }
+
+    @media screen and (max-width: 425px) {
+
     }
   }
 
