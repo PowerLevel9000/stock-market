b742090a7be7b57d7a43895669512e4ba472bb17
PowerLevel9000 <116451144+PowerLevel9000@users.noreply.github.com>
2023-03-24 17:59:36 +0530

    Test the Testing

---
+++ src/__test__/hello.test.jsx
@@ -0,0 +1,7 @@
+// import { screen, render } from "@testing-library/react";
+
+describe('testing the test: "joke"', () => {
+    test('you are joke', () => {
+        expect('talk is shit, show me  code').toEqual('talk is shit, show me  code')
+    })
+})
